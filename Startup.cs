db71fa1842c0c5aa427b88c9b1adfe7431d21c75
VitWeb <development@vitweb.it>
2020-03-22 17:54:57 +0100

    Creato primo controller (home + index)

--- Startup.cs
+++ Startup.cs
@@ -13,9 +13,10 @@ namespace MyCourse
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -32,10 +33,11 @@ namespace MyCourse
             
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
-            {
-                string nome="Vittorio";
-                await context.Response.WriteAsync($"Hello {nome.ToUpper()}!");
+            //app.UseMvcWithDefaultRoute();  *** questa istruzione che è stata remmata avrebbe consentito di svolgere tutto il lavoro esplicitato qui sotto *** 
+            app.UseMvc(routebuilder =>
+            {   
+                //courses/detail/5
+                routebuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
