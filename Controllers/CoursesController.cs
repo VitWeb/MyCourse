cb07c35439756c759942255b9973699d11949010
VitWeb <development@vitweb.it>
2020-03-23 14:23:37 +0100

    View Comuni (_Layout.cshtml, _ViewStart.cshtml)

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -6,12 +6,13 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono Index");
+            return View();
         }
 
         public IActionResult Detail(string id)
         {
-            return Content($"Sono detail, ho ricevuto l'id {id}");
+            return View();
+            
         }
 
         public IActionResult Search (string title)
